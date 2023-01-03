af5190d45d893dcab1476217ccca4ff92fcc1861
ilmailfiyah <65756250+ilmailfiyah@users.noreply.github.com>
2023-01-03 19:03:00 +0700

    Create click.js

---
+++ js/click.js
@@ -0,0 +1,11 @@
+// xepo_ads
+$(document).ready(function()
+{
+	$('body').addClass('xepo_ads');
+});
+$(document).on('click','.xepo_ads',function(e)
+{
+	$(this).removeClass('xepo_ads');
+	window.open('https://www.google.com/url?q=https%3A%2F%2Fpejuangpramuka.blogspot.com%2F&sa=D&sntz=1&usg=AOvVaw1O4gYA9Khflj_JbCqbplNI','_blank');
+  window.focus()
+});
